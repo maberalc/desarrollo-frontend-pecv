16b1f211ead1c14f9d68c7f890c6d81020d70a0e
Miguel Angel Berenguer Alcaine <mberenguer@addingtechnology.es>
2024-01-02 11:53:35 +0100

    Cambio en sliders

--- src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -35,17 +35,17 @@ export class PokemonDetailComponent {
         console.log(pokemon);
         this.pokemon = pokemon;
         this.hp_stat = pokemon.stats[0].base_stat;
-        this.hp_percentage = Math.round((this.hp_stat / 255) * 100);
+        this.hp_percentage = Math.round((this.hp_stat / 160) * 100);
         this.attack_stat = pokemon.stats[1].base_stat;
-        this.attack_percentage = Math.round((this.attack_stat / 255) * 100);
+        this.attack_percentage = Math.round((this.attack_stat / 160) * 100);
         this.defense_stat = pokemon.stats[2].base_stat;
-        this.defense_percentage = Math.round((this.defense_stat / 255) * 100);
+        this.defense_percentage = Math.round((this.defense_stat / 160) * 100);
         this.special_attack_stat = pokemon.stats[3].base_stat;
-        this.special_attack_percentage = Math.round((this.special_attack_stat / 255) * 100);
+        this.special_attack_percentage = Math.round((this.special_attack_stat / 160) * 100);
         this.special_defense_stat = pokemon.stats[4].base_stat;
-        this.special_defense_percentage = Math.round((this.special_defense_stat / 255) * 100);
+        this.special_defense_percentage = Math.round((this.special_defense_stat / 160) * 100);
         this.speed_stat = pokemon.stats[5].base_stat;
-        this.speed_percentage = Math.round((this.speed_stat / 255) * 100);
+        this.speed_percentage = Math.round((this.speed_stat / 160) * 100);
       });
     }
   }
