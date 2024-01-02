d136b5cbb9dd7bd2ff5fa2a8d814af17c453a963
Miguel Angel Berenguer Alcaine <mberenguer@addingtechnology.es>
2024-01-02 11:53:12 +0100

    Añadida animación en el listado

--- src/app/components/pokemons-list/pokemons-list.component.ts
+++ src/app/components/pokemons-list/pokemons-list.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { PokemonsService } from '../../services/pokemons.service';
 import { NamedAPIResource } from '../../models/pokemon.interface';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-pokemons-list',
   templateUrl: './pokemons-list.component.html',
-  styleUrl: './pokemons-list.component.css'
+  styleUrl: './pokemons-list.component.css',
+  animations: [
+    trigger('fadeIn', [
+      state(
+        'void',
+        style({
+          opacity: 0.2
+        })
+      ),
+      transition('void => *', animate(350))
+    ])
+  ]
 })
 export class PokemonsListComponent {
 
-  pokemons: NamedAPIResource[] = [];
+  pokemons: NamedAPIResource[] | undefined = undefined;
   count: number = 0;
   limit: number = 10;
   offset: number = 0;
@@ -26,6 +38,7 @@ export class PokemonsListComponent {
 
   onPageChange(event: any): void {
     console.log(event);
+    this.pokemons = undefined;
     this.limit = event.pageSize;
     this.offset = event.pageIndex * event.pageSize;
     this.pokemonsService.getPokemons(this.limit, this.offset).subscribe((pokemons) => {
